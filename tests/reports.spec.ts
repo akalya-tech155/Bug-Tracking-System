4f53fae4af2aa52969bdd75cb47e654b00005caa
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-03 18:04:17 +0530

    created report file

---
+++ tests/reports.spec.ts
@@ -0,0 +1,31 @@
+/*
+
+Generate reports.
+
+Examples
+
+Filter
+
+Status = Open
+
+Count tickets
+
+Filter
+
+Priority = Critical
+
+Verify number
+
+Export
+
+CSV
+
+Verify file downloaded.
+
+Playwright concepts
+
+download()
+filtering
+loops
+
+*/
