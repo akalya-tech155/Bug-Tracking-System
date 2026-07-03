7687c3dc1fe4e76d56ca5b144267db3b7f80ab42
akalya-tech155 <akalyavlab182@gmail.com>
2026-07-03 17:59:42 +0530

    created ticket status

---
+++ pages/ticketstatuspage.ts
@@ -0,0 +1,41 @@
+/*
+
+Statuses
+
+Open
+
+In Progress
+
+Fixed
+
+Testing
+
+Closed
+
+Rejected
+Test
+
+Open ticket
+
+Change status
+
+Open
+
+↓
+
+In Progress
+
+Verify badge
+
+In Progress
+
+Later
+
+In Progress
+
+↓
+
+Testing
+
+Verify
+*/
